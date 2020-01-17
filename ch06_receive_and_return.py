1a77252865f051713517606906eba28df16dcdf5
SfilD <pixie.winx.club@gmail.com>
2020-01-17 22:39:45 +0900

    17.01.2020 22:39

---
+++ ch06_receive_and_return.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Принимай - возвращай
+# Демонстрирует параметры и возвращаемые значения
+
+def display(message):
+    print(message)
+
+def give_me_five():
+    five = 5
+    return five
