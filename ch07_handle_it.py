9ab560ae47a0df2d0f505f81a08a810e2e8fa2f5
SfilD <pixie.winx.club@gmail.com>
2020-01-26 22:21:45 +0900

    26.01.2020 22:21 Win10 Desktop Home

--- ch07_handle_it.py
+++ ch07_handle_it.py
@@ -12,3 +12,11 @@ try:
 except ValueError:
     print("Похоже, это не число!")
 
+# обработка исключений нескольких разных типов
+print()
+for value in (None, "Привет!", "77", 21):
+    try:
+        print("Пытаюсь преобразовать в число: ", value, "-->", end=" ")
+        print(float(value))
+    except (TypeError, ValueError):
+        print("Похоже, это не число!")
