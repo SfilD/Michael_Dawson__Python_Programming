7dc33f9e0ad0f6e0a4a9c35d15f868051dd1f00c
SfilD <pixie.winx.club@gmail.com>
2020-02-13 16:49:10 +0900

    13.02.2020 16:48 Win10 Desktop Work

--- ch09_blackjack.py
+++ ch09_blackjack.py
@@ -50,3 +50,130 @@ class BJ_Hand(ch09_cards.Hand):
         for card in self.cards:
             t += card.value
         # определяем, есть ли туз на руках у игрока
+        contains_ace = False
+        for card in self.cards:
+            if card.value == BJ_Card.ACE_VALUE:
+                contains_ace = True
+        # если на руках есть туз и сумма очков не превышает 11, будем считать туз за 11 очков
+        if contains_ace and t <= 11:
+            # прибавить нужно лишь 10, потому что единица уже вошла в общую сумму
+            t += 10
+        return t
+
+    def is_busted(self):
+        return self.total > 21
+
+
+    class BJ_Player(BJ_Hand):
+        """Игрок в 'Блэк-джек'"""
+        def is_hitting(self):
+            response = ch09_games.ask_yes_no("\n" + self.name + ", будете брать ещё карты? (y/n): ")
+            return response == "y"
+
+        def bust(self):
+            print(self.name, "перебрал")
+            self.lose()
+
+        def lose(self):
+            print(self.name, "проиграл")
+
+        def win(self):
+            print(self.name, "выиграл")
+
+        def push(self):
+            print(self.name, "сыграл с компьютером вничью")
+
+
+    class BJ_Dealer(BJ_Hand):
+        """Дилер в игре 'Блэк-джек'"""
+        def is_hitting(self):
+            return self.total < 17
+
+        def bust(self):
+            print(self.name, "перебрал")
+
+        def flip_first_card(self):
+            first_card = self.cards[0]
+            first_card.flip()
+
+
+    class BJ_Game(object):
+        """Игра в 'Блэк-джек'"""
+        def __init__(self, names):
+            self.players = []
+            for name in names:
+                player = BJ_Player(name)
+                self.players.append(player)
+            self.dealer = BJ_Dealer("Dealer")
+            self.deck = BJ_Deck()
+            self.deck.populate()
+            self.deck.shuffle()
+
+        @property
+        def still_playing(self):
+            sp = []
+            for player in self.players:
+                if not player.is_busted():
+                    sp.append(player)
+            return sp
+
+        def __additional_cards(self, player):
+            while not player.is_busted() and player.is_hitting():
+                self.deck.deal([player])
+                print(player)
+                if player.is_busted():
+                    player.bust()
+
+        def play(self):
+            # сдача всем по 2 карты
+            self.deck.deal(self.players + [self.dealer], per_hand=2)
+            self.dealer.flip_first_card() # первая из карт, сданных дилеру, переворачивается рубашкой вверх
+            for player in self.players:
+                print(player)
+            print(self.dealer)
+            # сдача дополнительных карт игрокам
+            for player in self.players:
+                self.__additional_cards(player)
+            self.dealer.flip_first_card() # первая карта дилера раскрывается
+            if not self.still_playing:
+                # все игроки перебрали, покажем только "руку" дилера
+                print(self.dealer)
+            else:
+                # сдача дополнительных карт дилеру
+                print(self.dealer)
+                self.__additional_cards(self.dealer)
+                if self.dealer.is_busted():
+                    # выигрывают все, кто ещё остался в игре
+                    for player in self.still_playing:
+                        player.win()
+                else:
+                    # сворачиваем суммы очков у дилера и у игроков, оставшихся в игре
+                    for player in self.still_playing:
+                        if player.total > self.dealer.total:
+                            player.win()
+                        elif player.total < self.dealer.total:
+                            player.lose()
+                        else:
+                            player.push()
+            # удаление всех карт
+            for player in self.players:
+                player.clear()
+            self.dealer.clear()
+
+
+def main():
+    print("Добро пожаловать за игровой стол Блэк-джека!")
+    names = []
+    number = ch09_games.ask_number("Сколько всего игроков? (1 - 7): ", low=1, high=8)
+    for i in range(number):
+        name = input("Введите имя игрока: ")
+        names.append(name)
+    print()
+    game = BJ_Game(names)
+    again = None
+    while again != "n":
+        game.play()
+        again = ch09_games.ask_yes_no("Хотите сыграть ещё раз? ")
+
+
+main()
