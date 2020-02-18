4e6ada9a977667ec33ee54f8b3b84277539626ff
SfilD <pixie.winx.club@gmail.com>
2020-02-18 22:22:27 +0900

    18.02.2020 22:22 Win10 Desktop Home

---
+++ ch10_simple_gui.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Простейший GUI
+# Демонстрирует создание окна
+from tkinter import *
+
+# создание базового окна
+root = Tk()
+
+# изменение окна
+root.title("Простейший GUI")
+root.geometry("500x250")
+
+# старт событийного цикла
+root.mainloop()
